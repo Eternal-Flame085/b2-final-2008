6da2de51811c0fab89e35540b832406297b456ab
Roberto Basulto <roberto.basulto085@gmail.com>
2020-11-09 13:31:56 -0500

    Created controller method

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -3,4 +3,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:doctor_id])
   end
+
+  def remove_patient
+    doctor_patiant_relation = DoctorPatient.find_by(doctor_id: params[:doctor_id], patient_id: params[:patient_id])
+    doctor_patiant_relation.destroy
+
+    redirect_to "/doctors/#{params[:doctor_id]}"
+  end
 end
