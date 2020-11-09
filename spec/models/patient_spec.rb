c3927ef985fa41d755e88f6aed5c2b98140eeeee
Roberto Basulto <roberto.basulto085@gmail.com>
2020-11-09 13:52:21 -0500

    Created model that returns patients ordered by age from olders to youngest

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe Patient, type: :model do
   it {should have_many :doctor_patients}
   it {should have_many :doctors}
+
+  describe 'class methods' do
+    it ".patients_by_age" do
+      @patient_1 = Patient.create!(name: 'Katie Bryce', age: 24)
+      @patient_2 = Patient.create!(name: 'Denny Duquette', age: 39)
+      @patient_3 = Patient.create!(name: 'Rebecca Pope', age: 32)
+      @patient_4 = Patient.create!(name: 'Zola Shepherd', age: 2)
+
+      expected = [@patient_2, @patient_3, @patient_1, @patient_4]
+      expect(Patient.patients_by_age).to eq(expected)
+    end
+  end
 end
